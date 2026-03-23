fad8e998a0b912f86caf85d47563cd07ae8945bc
TVR DEVLOPER <pammiali35@gmail.com>
2026-03-23 18:14:32 +0530

    Add files via upload

--- helper/set.py
+++ helper/set.py
@@ -1,1 +1,48 @@
+from typing import Dict, List
 
+
+
+def escape_invalid_curly_brackets(text: str, valids: List[str]) -> str:
+    new_text = ""
+    idx = 0
+    while idx < len(text):
+        if text[idx] == "{":
+            if idx + 1 < len(text) and text[idx + 1] == "{":
+                idx += 2
+                new_text += "{{{{"
+                continue
+            else:
+                success = False
+                for v in valids:
+                    if text[idx:].startswith('{' + v + '}'):
+                        success = True
+                        break
+                if success:
+                    new_text += text[idx: idx + len(v) + 2]
+                    idx += len(v) + 2
+                    continue
+                else:
+                    new_text += "{{"
+
+        elif text[idx] == "}":
+            if idx + 1 < len(text) and text[idx + 1] == "}":
+                idx += 2
+                new_text += "}}}}"
+                continue
+            else:
+                new_text += "}}"
+
+        else:
+            new_text += text[idx]
+        idx += 1
+
+    return new_text
+
+
+
+
+
+# Tanveer Developer 
+# Please Don't Remove Credit 🥺
+# Telegram Channel @TVR_BOTS
+# Developer @SONY_YAY_0000 & @T4NVEER_HERE
